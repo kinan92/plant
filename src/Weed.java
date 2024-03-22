5a1e0a53201f90a426afe80c024e86fa7bfa1ae2
Alex04 <153679198+Alexoo1232@users.noreply.github.com>
2024-03-22 15:37:43 +0100

    asd

---
+++ src/Weed.java
@@ -0,0 +1,4 @@
+package src;
+public class Weed {
+    private int days;
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}

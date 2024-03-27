63824080334568b647d7a24dae51b5a70d343273
Ellie <viris541@gmail.com>
2024-03-27 13:33:46 +0100

    Ikoner lagts till i Main GUI.

--- src/boundary/mainGUI.java
+++ src/boundary/mainGUI.java
@@ -11,10 +11,14 @@ public class mainGUI extends JFrame {
     int width = 550;
     int height = 450;
     private ImageIcon elefantöra = new ImageIcon("images/elefantora.png");
+    private ImageIcon skiphour = new ImageIcon("images/skiphour.png");
+    private ImageIcon storage = new ImageIcon("images/storage.png");
+    private ImageIcon vacationImage = new ImageIcon("images/vacation.png");
+    private ImageIcon widgetImage = new ImageIcon("images/widget.png");
     public mainGUI()
     {
         setLayout(new BorderLayout());
-        setTitle("Test");
+        setTitle("Virtual Plant Widgets");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(width, height);
         setLocationRelativeTo(null);
@@ -34,28 +38,80 @@ public class mainGUI extends JFrame {
         JPanel sideButtons = new JPanel();
         sideButtons.setBackground(Color.GRAY);
         sideButtons.setPreferredSize(new Dimension(230, (height / 5) * 3));
+        sideButtons.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
 
-        JButton getPlant = new JButton("Plant Collection");
-        getPlant.setPreferredSize(new Dimension(190, 35));
+        //Creates Plant Storage button
+        JButton getPlant = new JButton();
+        getPlant.setBorder(BorderFactory.createEmptyBorder());
+        getPlant.setContentAreaFilled(false);
+        getPlant.setIcon(storage);
+        getPlant.setPreferredSize(new Dimension(208, 60));
+
+        //Sets location for Plant Storage button
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 0;
+
+        //Adds Plant Storage button to panel and adds actionlistener
+        sideButtons.add(getPlant, c);
         getPlant.addActionListener(l -> getPlantPressed());
+        getPlant.setRolloverEnabled(true);
+        getPlant.setRolloverIcon(new ImageIcon("images/storage_hover.png"));
 
-        JButton widget = new JButton("Place Widget");
-        widget.setPreferredSize(new Dimension(190, 35));
+        //Creates Widget button
+        JButton widget = new JButton();
+        widget.setBorder(BorderFactory.createEmptyBorder());
+        widget.setContentAreaFilled(false);
+        widget.setIcon(widgetImage);
+        widget.setPreferredSize(new Dimension(208, 60));
+
+        //Sets location for Widget button
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 1;
+
+        //Adds Widget button to panel and adds actionlistener
+        sideButtons.add(widget, c);
         widget.addActionListener(l -> widgetPressed());
+        widget.setRolloverEnabled(true);
+        widget.setRolloverIcon(new ImageIcon("images/widget_hover.png"));
 
-        JButton skipHour = new JButton("Skip Hour");
-        skipHour.setPreferredSize(new Dimension(190, 35));
+        //Creates Skip Hour button
+        JButton skipHour = new JButton();
+        skipHour.setBorder(BorderFactory.createEmptyBorder());
+        skipHour.setContentAreaFilled(false);
+        skipHour.setIcon(skiphour);
+        skipHour.setPreferredSize(new Dimension(208, 60));
+
+        //Sets location for Skip Hour button
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 2;
+
+        //Adds Skip Hour button to panel and adds actionlistener
+        sideButtons.add(skipHour, c);
         skipHour.addActionListener(l -> skipHourPressed());
+        skipHour.setRolloverEnabled(true);
+        skipHour.setRolloverIcon(new ImageIcon("images/skiphour_hover.png"));
 
-        JButton vacation = new JButton("Vacation mode");
-        vacation.setPreferredSize(new Dimension(190, 35));
+        //Creates Vacation button
+        JButton vacation = new JButton();
+        vacation.setBorder(BorderFactory.createEmptyBorder());
+        vacation.setContentAreaFilled(false);
+        vacation.setIcon(vacationImage);
+        vacation.setPreferredSize(new Dimension(208, 60));
+
+        //Sets location for Vacation button
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 3;
+
+        //Adds Vacation button to panel and adds actionlistener
+        sideButtons.add(vacation, c);
         vacation.addActionListener(l -> vacationPressed());
-
-        sideButtons.add(getPlant);
-        sideButtons.add(widget);
-        sideButtons.add(skipHour);
-        sideButtons.add(vacation);
-
+        vacation.setRolloverEnabled(true);
+        vacation.setRolloverIcon(new ImageIcon("images/vacation_hover.png"));
 
         return sideButtons;
     }
@@ -83,11 +139,22 @@ public class mainGUI extends JFrame {
     public JPanel nameView()
     {
         JPanel nameView = new JPanel();
-        nameView.setPreferredSize(new Dimension(width / 2, (height / 10)));
-        nameView.setLayout(new FlowLayout(FlowLayout.LEFT));
-        JLabel plantName = new JLabel("Test");
-        plantName.setFont(new Font("Montserrat", Font.PLAIN, 18));
-        nameView.add(plantName);
+        nameView.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 0;
+        JLabel plantName = new JLabel("Bob");
+        plantName.setFont(new Font("Montserrat", Font.PLAIN, 32));
+        nameView.add(plantName, c);
+
+        c.weightx = 0;
+        c.gridx = 0;
+        c.gridy = 1;
+        JLabel plantSpecies = new JLabel("Species: Elefantöra");
+        plantName.setFont(new Font("Montserrat", Font.PLAIN, 24));
+        nameView.add(plantSpecies, c);
 
         return nameView;
     }
@@ -100,33 +167,20 @@ public class mainGUI extends JFrame {
         plantView.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        JLabel plantName = new JLabel("Bob");
-        plantName.setFont(new Font("Montserrat", Font.PLAIN, 24));
-
-        JLabel plantSpecies = new JLabel("Species: Elefantöra");
-        plantName.setFont(new Font("Montserrat", Font.PLAIN, 16));
-
-        c.fill = GridBagConstraints.VERTICAL;
+        c.weightx = 0;
         c.gridx = 0;
         c.gridy = 0;
-        plantView.add(plantName, c);
+        plantView.add(nameView(), c);
 
-        c.fill = GridBagConstraints.VERTICAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 1;
-        plantView.add(plantSpecies, c);
-
-        c.weightx = 0.5;
+        c.weightx = 1;
         c.gridx = 0;
         c.gridy = 2;
         JLabel plantImage = new JLabel(elefantöra);
         plantView.add(plantImage, c);
 
-        c.weightx = 1;
+        c.weightx = 0;
         c.gridx = 0;
         c.gridy = 3;
-        c.anchor = GridBagConstraints.LAST_LINE_END;
         JPanel plantCare = plantCare();
         plantView.add(plantCare, c);
 
@@ -136,27 +190,36 @@ public class mainGUI extends JFrame {
     private JPanel plantCare()
     {
         JPanel plantView = new JPanel();
-        plantView.setPreferredSize(new Dimension(256, (height / 10)));
+        plantView.setPreferredSize(new Dimension(256, 55));
         plantView.setBackground(Color.RED);
         plantView.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        JButton waterPlant = new JButton("Water plant");
-        waterPlant.setFont(new Font("Montserrat", Font.PLAIN, 16));
+        /*JButton waterPlant = new JButton("Water plant");
+        waterPlant.setFont(new Font("Montserrat", Font.PLAIN, 16));*/
 
-        JButton feedPlant = new JButton("Feed plant");
-        feedPlant.setFont(new Font("Montserrat", Font.PLAIN, 16));
+        JButton waterPlant = new JButton();
+        waterPlant.setBorder(BorderFactory.createEmptyBorder());
+        waterPlant.setContentAreaFilled(false);
+        waterPlant.setIcon(new ImageIcon("images/water.png"));
+        waterPlant.setPreferredSize(new Dimension(45, 45));
 
-        c.fill = GridBagConstraints.HORIZONTAL;
+        plantView.add(waterPlant, c);
+        waterPlant.addActionListener(l -> getPlantPressed());
+        waterPlant.setRolloverEnabled(true);
+        waterPlant.setRolloverIcon(new ImageIcon("images/water_hover.png"));
+
+        c.weightx = 0;
+        c.weighty = 0;
         c.gridx = 0;
         c.gridy = 0;
         plantView.add(waterPlant, c);
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weighty = 0.5;
+        c.weightx = 1;
+        c.weighty = 0;
         c.gridx = 1;
         c.gridy = 0;
-        plantView.add(feedPlant, c);
+        plantView.add(Box.createGlue(), c);
 
         return plantView;
     }

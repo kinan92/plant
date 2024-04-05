1b0c9db9dbbb56d263ffe3a69ca813ec9857f9ca
Ellie <viris541@gmail.com>
2024-04-05 16:55:06 +0200

    Update MainFrame.java
    La till funktionalitet på create plant button och gjorde så att fönstret hamnar i mitten av användarens skärm.

--- src/boundary/MainFrame.java
+++ src/boundary/MainFrame.java
@@ -6,29 +6,30 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame {
-	JButton creatPlantButton = new JButton("Creat Plant");
-	JButton getAPlantButton = new JButton("open Last Plant");
+	JButton createPlantButton = new JButton("Create Plant");
+	JButton getAPlantButton = new JButton("Open Last Plant");
 	GroupLayout groupLayout = new GroupLayout(getContentPane());
 
 	/**
 	 * Create the application.
 	 */
 	public MainFrame() {
-
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(false);
 		initialize();
 	}
 
-	private void clickCreatPlantButton() {
-		getAPlantButton.addActionListener(new ActionListener() {
+	private void clickCreatePlantButton() {
+		createPlantButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
+				ChoosePlantFrame choosePlantFrame = new ChoosePlantFrame();
+				dispose();
 			}
 		});
 	}
@@ -37,10 +38,6 @@ public class MainFrame extends JFrame {
 		getAPlantButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				 mainGUI maingui = new mainGUI();
-				 
-				 
-				
-				 
 				 dispose();	 
 			}
 		});
@@ -48,20 +45,19 @@ public class MainFrame extends JFrame {
 
 
 	private void initialize() {
-
-		setBounds(100, 100, 306, 226);
+		setLocationRelativeTo(null);
+		setSize(306, 226);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		SetgroupLayoutVertical();
 		SetgroupLayoutHorizontal();
 		clickGetAPlantButton();
-
-		
+		clickCreatePlantButton();
 	}
 	
 	private void SetgroupLayoutVertical() {
 		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup().addGap(41)
-						.addComponent(creatPlantButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+						.addComponent(createPlantButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
 						.addGap(18)
 						.addComponent(getAPlantButton, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
 						.addContainerGap(43, Short.MAX_VALUE)));
@@ -74,7 +70,7 @@ public class MainFrame extends JFrame {
 						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
 								.addComponent(getAPlantButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
 										GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(creatPlantButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 102,
+								.addComponent(createPlantButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 102,
 										Short.MAX_VALUE))
 						.addContainerGap(99, Short.MAX_VALUE)));
 		

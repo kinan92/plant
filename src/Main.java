accb0ee9d775b35f9724e73b988955345318b9c5
PetriNarhi <pideli94@gmail.com>
2024-05-15 20:31:16 +0200

    Slutförde javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 import controller.Controller;
 
+/**
+ * Entry point of the program
+ * @author Kinan
+ * */
 public class Main {
 
+	/**
+	 * Starts the program
+	 * @param args String[]
+	 * @author Kinan
+	 * */
 	public static void main(String[] args) {
 		 Controller controller = new Controller();
 

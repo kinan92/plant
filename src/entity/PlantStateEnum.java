d46462342fda01b47672a00f0e81d3943af792f9
Alex04 <153679198+Alexoo1232@users.noreply.github.com>
2024-05-29 02:24:03 +0100

    Reapply "Reapply "Reapply "Reapply "Merge branch 'stateV5'""""
    This reverts commit 8315c260d722343fa66fbab647cd3ec1160d31a3.

--- src/entity/PlantStateEnum.java
+++ src/entity/PlantStateEnum.java
@@ -1,15 +1,35 @@
 package entity;
 
 public enum PlantStateEnum {
-	
-	little("Little"),
-	big("Big"),
-	dead("dead");
+
+	small("Seedling"),
+	medium("Growing"),
+	large("Fully grown"),
+	smallDead("Dead"),
+	mediumDead("Dead"),
+	largeDead("Dead");
 
 	private final String state;
 
 	PlantStateEnum(String state) {
 		this.state = state;
+	}
 
+	public String getState()
+	{
+		return state;
+	}
+
+	public PlantStateEnum getDeadState() {
+		switch (this){
+			case small:
+				return smallDead;
+			case medium:
+				return mediumDead;
+			case large:
+				return largeDead;
+			default:
+				return this;
+		}
 	}
 }
